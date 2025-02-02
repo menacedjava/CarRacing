af693ee47f3331c1e6ea781d59c51fa79cc06afe
Madumarov Davron <menaceteacher@gmail.com>
2025-02-03 00:49:45 +0500

    add

--- src/main/java/org/example/CarRacingGame.java
+++ src/main/java/org/example/CarRacingGame.java
@@ -63,94 +63,94 @@ public class CarRacingGame extends JPanel {
             obstaclesY[i] = rand.nextInt(BOARD_HEIGHT / 2);
         }
     }
-//
-//
-//    public void moveObstacles() {
-//        for (int i = 0; i < obstaclesY.length; i++) {
-//            obstaclesY[i] += speed;
-//            if (obstaclesY[i] > BOARD_HEIGHT) {
-//                obstaclesY[i] = -OBSTACLE_HEIGHT;
-//                obstaclesX[i] = new Random().nextInt(roadWidth - OBSTACLE_WIDTH) + roadX;
-//            }
-//        }
-//    }
-//
-//
-//    public boolean checkCollision() {
-//        for (int i = 0; i < obstaclesX.length; i++) {
-//
-//            if (obstaclesY[i] + OBSTACLE_HEIGHT > carY && obstaclesY[i] < carY + CAR_HEIGHT &&
-//                    obstaclesX[i] + OBSTACLE_WIDTH > carX && obstaclesX[i] < carX + CAR_WIDTH) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//
-//
-//    public void updateGame() {
-//        if (gameOver) return;
-//
-//        moveObstacles();
-//
-//        if (checkCollision()) {
-//            gameOver = true;
-//        }
-//    }
-//
-//
-//    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//
-//
-//        updateGame();
-//
-//        if (gameOver) {
-//            g.setColor(Color.RED);
-//            g.setFont(new Font("Arial", Font.BOLD, 40));
-//            g.drawString("Game Over!", BOARD_WIDTH / 4, BOARD_HEIGHT / 2);
-//            return;
-//        }
-//
-//
-//        g.setColor(Color.GRAY);
-//        g.fillRect(roadX, 0, roadWidth, BOARD_HEIGHT);
-//
-//
-//        g.setColor(Color.WHITE);
-//        for (int i = 0; i < BOARD_HEIGHT; i += 40) {
-//            g.fillRect(roadX + roadWidth / 2 - 5, i, 10, 20); // O'rta bo'g'in
-//        }
-//
-//
-//        g.setColor(Color.BLUE);
-//        g.fillRect(carX, carY, CAR_WIDTH, CAR_HEIGHT);
-//
-//
-//        g.setColor(Color.RED);
-//        for (int i = 0; i < obstaclesX.length; i++) {
-//            g.fillRect(obstaclesX[i], obstaclesY[i], OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
-//        }
-//    }
-//
-//    public static void main(String[] args) {
-//        JFrame frame = new JFrame("Car Racing Game");
-//        CarRacingGame gamePanel = new CarRacingGame();
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        frame.add(gamePanel);
-//        frame.pack();
-//        frame.setLocationRelativeTo(null);
-//        frame.setVisible(true);
-//
-//
-//        while (!gamePanel.gameOver) {
-//            try {
-//                Thread.sleep(10);
-//                gamePanel.repaint();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//    }
+
+
+    public void moveObstacles() {
+        for (int i = 0; i < obstaclesY.length; i++) {
+            obstaclesY[i] += speed;
+            if (obstaclesY[i] > BOARD_HEIGHT) {
+                obstaclesY[i] = -OBSTACLE_HEIGHT;
+                obstaclesX[i] = new Random().nextInt(roadWidth - OBSTACLE_WIDTH) + roadX;
+            }
+        }
+    }
+
+
+    public boolean checkCollision() {
+        for (int i = 0; i < obstaclesX.length; i++) {
+
+            if (obstaclesY[i] + OBSTACLE_HEIGHT > carY && obstaclesY[i] < carY + CAR_HEIGHT &&
+                    obstaclesX[i] + OBSTACLE_WIDTH > carX && obstaclesX[i] < carX + CAR_WIDTH) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public void updateGame() {
+        if (gameOver) return;
+
+        moveObstacles();
+
+        if (checkCollision()) {
+            gameOver = true;
+        }
+    }
+
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+
+        updateGame();
+
+        if (gameOver) {
+            g.setColor(Color.RED);
+            g.setFont(new Font("Arial", Font.BOLD, 40));
+            g.drawString("Game Over!", BOARD_WIDTH / 4, BOARD_HEIGHT / 2);
+            return;
+        }
+
+
+        g.setColor(Color.GRAY);
+        g.fillRect(roadX, 0, roadWidth, BOARD_HEIGHT);
+
+
+        g.setColor(Color.WHITE);
+        for (int i = 0; i < BOARD_HEIGHT; i += 40) {
+            g.fillRect(roadX + roadWidth / 2 - 5, i, 10, 20); // O'rta bo'g'in
+        }
+
+
+        g.setColor(Color.BLUE);
+        g.fillRect(carX, carY, CAR_WIDTH, CAR_HEIGHT);
+
+
+        g.setColor(Color.RED);
+        for (int i = 0; i < obstaclesX.length; i++) {
+            g.fillRect(obstaclesX[i], obstaclesY[i], OBSTACLE_WIDTH, OBSTACLE_HEIGHT);
+        }
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Car Racing Game");
+        CarRacingGame gamePanel = new CarRacingGame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(gamePanel);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+
+        while (!gamePanel.gameOver) {
+            try {
+                Thread.sleep(10);
+                gamePanel.repaint();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
